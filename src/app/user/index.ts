0d5083059835e6213752669b148e8676e470d8e7
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-03-09 16:18:05 +0530

    adding service to user module and core

--- src/app/user/index.ts
+++ src/app/user/index.ts
@@ -3,12 +3,15 @@ import { UserRoutes } from './user.routes';
 import { NgModule } from '@angular/core';
 
 // components
-import { OverviewComponent } from './overview/overview.component';
-import { OrdersComponent } from './orders/orders.component';
-import { OrderListItemComponent } from './orders/order-list-item/order-list-item.component';
-import { ReturnsComponent } from './returns/returns.component';
-import { ReturnListItemComponent } from './returns/return-list-item/return-list-item.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { OrderListItemComponent } from './components/orders/order-list-item/order-list-item.component';
+import { ReturnsComponent } from './components/returns/returns.component';
+import { ReturnListItemComponent } from './components/returns/return-list-item/return-list-item.component';
 import { UserComponent } from './user.component';
+
+// services
+import { UserService } from './services/user.service';
 
 import { UserRoutes as routes } from './user.routes';
 
@@ -32,6 +35,9 @@ import { UserRoutes as routes } from './user.routes';
     // ReturnsComponent,
     // ReturnListItemComponent,
     
+  ],
+  providers: [
+    UserService
   ],
   imports: [
     RouterModule.forChild(routes),
