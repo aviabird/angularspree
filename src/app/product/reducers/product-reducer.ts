ac092b979a746a60a7f7321a675a7eaa2b608401
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-03-10 12:50:19 +0530

    bug fixes

--- src/app/product/reducers/product-reducer.ts
+++ src/app/product/reducers/product-reducer.ts
@@ -5,16 +5,16 @@ import { Action, ActionReducer } from '@ngrx/store';
 
 export const initialState: ProductState = new ProductStateRecord() as ProductState;
 
-export const productReducer: ActionReducer<ProductState> = (state: ProductState = initialState, { type, payload }: Action): ProductState => {
+export const productReducer: ActionReducer<ProductState> =
+(state: ProductState = initialState, { type, payload }: Action): ProductState => {
   switch (type) {
 
-    case ProductActions.GET_PRODUCT_DETAIL_SUCCESS: {
+    case ProductActions.GET_PRODUCT_DETAIL_SUCCESS:
       Object.assign({}, state, {
         selectedProduct: payload
       });
 
       return state;
-    }
 
     // case ActionTypes.CLEAR_SELECTED_PRODUCT: {
     //   return Object.assign({}, state, {
@@ -26,20 +26,21 @@ export const productReducer: ActionReducer<ProductState> = (state: ProductState 
 
     case ProductActions.GET_ALL_PRODUCTS:
       const products: Product[] = payload;
-      const productIds: string[] = products.map(product => product.id);
-      const productEntities = payload.reduce((products: { [id: string]: Product }, product: Product) => {
-        Object.assign(products, {
+      const productIds: number[] = products.map(product => product.id);
+      const productEntities = payload.reduce((_products: { [id: string]: Product }, product: Product) => {
+        Object.assign(_products, {
           [product.id]: product
         });
       }, {});
       state.merge({
         productIds: productIds,
         productEntities: productEntities
-      })
+      });
       return state;
+
     // case "":
     //     return state;
     default:
       return state;
   }
-}
+};
