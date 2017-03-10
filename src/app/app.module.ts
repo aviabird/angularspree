4ab2dcc70cbb3d71fb24e3637696975601203495
voidzero <kartikjagdale11@gmail.com>
2017-03-10 13:42:12 +0530

    Remove Restangualr as not comaptible with aot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpModule } from '@angular/http';
 import { AccordionModule } from 'primeng/primeng';     // accordion and accordion tab
 import { MenuItem } from 'primeng/primeng';            // api
 import { RouterModule } from '@angular/router';
-import { RestangularModule } from 'ng2-restangular';
-import { RestangularConfigFactory } from './rest-angular-config';
 
 // Components
 import { AppComponent } from './app.component';
@@ -20,7 +18,7 @@ import { LayoutModule } from './layout/index';
 import { CartModule } from './cart/index';
 import { ProductModule } from './product/index';
 import { CoreModule } from './core/index';
-import { StoreModule } from "@ngrx/store";
+import { StoreModule } from '@ngrx/store';
 import { reducer } from './app.state';
 // AppState
 
@@ -31,8 +29,6 @@ import { reducer } from './app.state';
   imports: [
     RouterModule.forRoot(routes),
     StoreModule.provideStore(reducer),
-    // Importing RestangularModule and making default configs for restanglar
-    RestangularModule.forRoot(RestangularConfigFactory),
     BrowserModule,
     FormsModule,
     HttpModule,
