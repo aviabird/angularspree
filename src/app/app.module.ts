68faef7846f6021454da7c1b3486f189f1f19236
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-03-10 14:56:18 +0530

    added bootstrap and removed primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AccordionModule } from 'primeng/primeng';     // accordion and accordion tab
-import { MenuItem } from 'primeng/primeng';            // api
 import { RouterModule } from '@angular/router';
 
 // Components
