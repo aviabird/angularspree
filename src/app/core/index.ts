ef3e31d1ad7793355cbc39423bda3904801a95e8
voidzero <kartikjagdale11@gmail.com>
2017-03-10 15:17:06 +0530

    fix http service

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { ProductDummyService } from './services/product-dummy.service';
 import { HttpModule, XHRBackend, RequestOptions, Http } from '@angular/http';
 import { DropdownModule } from 'ng2-bootstrap/dropdown';
+import { HttpModule, XHRBackend, RequestOptions, Http } from '@angular/http';
 
 // Components
 
