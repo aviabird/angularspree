ad0ee12024dd176194d41692ebdf7b669ad7009b
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-03-11 14:37:53 +0530

    merge fix

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { ProductDummyService } from './services/product-dummy.service';
 import { HttpModule, XHRBackend, RequestOptions, Http } from '@angular/http';
 import { DropdownModule } from 'ng2-bootstrap/dropdown';
-import { HttpModule, XHRBackend, RequestOptions, Http } from '@angular/http';
-import { DropdownModule } from 'ng2-bootstrap/dropdown';
 
 // Components
 
