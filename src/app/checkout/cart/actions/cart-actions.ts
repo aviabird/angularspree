d9423b5edf8bc4f9837362304b36bcf6d8250b30
Chandra Shekhar <shekharindian099@gmail.com>
2017-03-13 23:34:10 +0530

    add cart actions

---
+++ src/app/checkout/cart/actions/cart-actions.ts
@@ -0,0 +1,37 @@
+import { Action } from '@ngrx/store';
+import { LineItem } from './../../../core/models/line_item';
+
+export class CartActions {
+  static ADD_LINE_ITEM = 'ADD_LINE_ITEM';
+  static REMOVE_LINE_ITEM = 'REMOVE_LINE_ITEM';
+  static CHANGE_LINE_ITEM_QUANTITY = 'CHANGE_LINE_ITEM_QUANTITY';
+  static PLACE_ORDER = 'PLACE_ORDER';
+
+  addLineItem(lineItem: LineItem): Action {
+    return {
+      type: CartActions.ADD_LINE_ITEM,
+      payload: lineItem
+    };
+  }
+
+  removeLineItem(lineItem: LineItem): Action {
+    return {
+      type: CartActions.REMOVE_LINE_ITEM,
+      payload: lineItem
+    };
+  }
+
+  changeLineItemQuantity(quantity: number): Action {
+    return {
+      type: CartActions.CHANGE_LINE_ITEM_QUANTITY,
+      payload: quantity
+    };
+  }
+
+  placeOrder(): Action {
+    return { type: CartActions.PLACE_ORDER };
+  }
+
+}
+
+
