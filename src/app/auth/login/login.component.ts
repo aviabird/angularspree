5de905c29f383f0ab50163b1124041df9bd596d0
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-03-13 12:07:03 +0530

    added login ui

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment.prod';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  title = environment.AppName;
 
   constructor() { }
 
