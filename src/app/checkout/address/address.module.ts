aa2fad8471ac18a3bc4696bb673ed406b0df3a2a
Chandra Shekhar <shekharindian099@gmail.com>
2017-03-17 17:18:48 +0530

    basic delivery options show on address component

--- src/app/checkout/address/address.module.ts
+++ src/app/checkout/address/address.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { AddressService } from './services/address.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddressComponent } from './address.component';
@@ -5,16 +6,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AddAddressComponent } from './add-address/add-address.component';
 import { DeliveryAddressComponent } from './delivery-address/delivery-address.component';
+import { DeliveryOptionsComponent } from './delivery-options/delivery-options.component';
 
 
 @NgModule({
   declarations: [
     AddressComponent,
     AddAddressComponent,
-    DeliveryAddressComponent
+    DeliveryAddressComponent,
+    DeliveryOptionsComponent
   ],
   exports: [],
   imports: [
+    RouterModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule
