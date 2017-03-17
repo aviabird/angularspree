e9a92d053a686c4b18566f3399bcebf4c9c58e92
Chandra Shekhar <shekharindian099@gmail.com>
2017-03-17 14:49:00 +0530

    add delivery address component

--- src/app/checkout/address/address.module.ts
+++ src/app/checkout/address/address.module.ts
@@ -4,12 +4,14 @@ import { AddressComponent } from './address.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AddAddressComponent } from './add-address/add-address.component';
+import { DeliveryAddressComponent } from './delivery-address/delivery-address.component';
 
 
 @NgModule({
   declarations: [
     AddressComponent,
-    AddAddressComponent
+    AddAddressComponent,
+    DeliveryAddressComponent
   ],
   exports: [],
   imports: [
---
+++ src/app/checkout/address/delivery-address/delivery-address.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-address',
+  templateUrl: './delivery-address.component.html',
+  styleUrls: ['./delivery-address.component.scss']
+})
+export class DeliveryAddressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
