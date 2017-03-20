5def8d7fe76624e37ab9a23c0dbe65a78246bb96
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-03-20 09:23:24 +0530

    adding order detail

---
+++ src/app/user/components/orders/order-detail/order-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-detail',
+  templateUrl: './order-detail.component.html',
+  styleUrls: ['./order-detail.component.scss']
+})
+export class OrderDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
