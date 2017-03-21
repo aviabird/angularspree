c0d96641d3048331323644890ce082105cf492ff
Chandra Shekhar <shekharindian099@gmail.com>
2017-03-21 13:04:38 +0530

    add checkout cart myntra ui

--- src/app/checkout/cart/cart.component.ts
+++ src/app/checkout/cart/cart.component.ts
@@ -14,36 +14,17 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit, OnDestroy {
+export class CartComponent implements OnInit {
 
-  stateSub$: Subscription;
-  orderState: string;
   totalCartValue$: Observable<number>;
-  totalCartItems: Observable<number>;
+  totalCartItems$: Observable<number>;
 
-  constructor(private store: Store<AppState>,
-    private actions: CheckoutActions,
-    private checkoutService: CheckoutService,
-    private router: Router) {
-      this.totalCartValue$ = this.store.select(getTotalCartValue);
-      this.stateSub$ = this.store.select(getOrderState)
-        .subscribe(state => this.orderState = state);
+  constructor(private store: Store<AppState>) {
+    this.totalCartValue$ = this.store.select(getTotalCartValue);
+    this.totalCartItems$ = this.store.select(getTotalCartItems);
   }
 
   ngOnInit() {
-    this.totalCartItems = this.store.select(getTotalCartItems);
-  }
-
-  placeOrder() {
-    if (this.orderState === 'cart') {
-      this.checkoutService.changeOrderState()
-        .subscribe();
-    }
-    this.router.navigate(['/checkout', 'address']);
-  }
-
-  ngOnDestroy() {
-    this.stateSub$.unsubscribe();
   }
 
 }
--- src/app/checkout/cart/components/order-total-summary/order-total-summary.component.ts
+++ src/app/checkout/cart/components/order-total-summary/order-total-summary.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { getOrderState } from './../../../reducers/selectors';
+import { Router } from '@angular/router';
+import { CheckoutService } from './../../../../core/services/checkout.service';
+import { CheckoutActions } from './../../../actions/checkout.actions';
+import { AppState } from './../../../../interfaces';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-order-total-summary',
   templateUrl: './order-total-summary.component.html',
   styleUrls: ['./order-total-summary.component.scss']
 })
-export class OrderTotalSummaryComponent implements OnInit {
+export class OrderTotalSummaryComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  stateSub$: Subscription;
+  orderState: string;
+  @Input() totalCartValue: number;
+
+
+  constructor(private store: Store<AppState>,
+    private actions: CheckoutActions,
+    private checkoutService: CheckoutService,
+    private router: Router) {
+  this.stateSub$ = this.store.select(getOrderState)
+    .subscribe(state => this.orderState = state);
+  }
 
   ngOnInit() {
+  }
+
+  placeOrder() {
+    if (this.orderState === 'cart') {
+      this.checkoutService.changeOrderState()
+        .subscribe();
+    }
+    this.router.navigate(['/checkout', 'address']);
+  }
+
+  ngOnDestroy() {
+    this.stateSub$.unsubscribe();
   }
 
 }
