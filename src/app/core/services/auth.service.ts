a7e39ea5e19eedf022269253ff75d740e4e65d1c
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-03-24 17:32:24 +0530

    bug fixes for notify

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -24,10 +24,19 @@ export class AuthService {
       'spree/login.json',
       { spree_user: data }
     ).map((res: Response) => {
-      // Setting token after login
-      this.setTokenInLocalStorage(res.json());
-      this.store.dispatch(this.actions.loginSuccess());
-      return res.json();
+      data = res.json();
+      if (!data.error) {
+        // Setting token after login
+        this.setTokenInLocalStorage(data);
+        this.store.dispatch(this.actions.loginSuccess());
+      } else {
+        this.http.loading.next({
+          loading: false,
+          hasError: true,
+          hasMsg: 'Please enter valid Credentials'
+        });
+      }
+      return data;
     });
     // catch should be handled here with the http observable
     // so that only the inner obs dies and not the effect Observable
@@ -41,9 +50,18 @@ export class AuthService {
       'api/account',
       { spree_user: data }
     ).map((res: Response) => {
-      // Setting token after login
-      this.setTokenInLocalStorage(res.json());
-      this.store.dispatch(this.actions.loginSuccess());
+      data = res.json();
+      if (!data.errors) {
+        // Setting token after login
+        this.setTokenInLocalStorage(res.json());
+        this.store.dispatch(this.actions.loginSuccess());
+      } else {
+        this.http.loading.next({
+          loading: false,
+          hasError: true,
+          hasMsg: 'Please enter valid Credentials'
+        });
+      }
       return res.json();
     });
     // catch should be handled here with the http observable
