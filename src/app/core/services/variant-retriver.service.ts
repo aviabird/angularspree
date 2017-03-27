dd2adfae5c5307659aea0d8c660eee99d61ab903
voidzero <kartikjagdale11@gmail.com>
2017-03-27 12:25:57 +0530

    add comments to varaint parser

--- src/app/core/services/variant-retriver.service.ts
+++ src/app/core/services/variant-retriver.service.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 
 /**
  * Note: This Service has a Lot of
@@ -27,6 +28,11 @@ export class VariantRetriverService {
    * due to prod-build error for constructor,
    * currently taking params from function;
    * TODO: fix this issue
+   * @param:
+   * currentSelectedOptions: { tsize: "small", tcolor: "red" }
+   * customOptionTypesHash: {tshirt-size: Object, tshirt-color: Object}
+   * currSelectedOption: { key: "Small", value: Object } => One that is recently selected;
+   * product: Product
    */
   getVariant(currentSelectedOptions: any,
     customOptionTypesHash: any,
@@ -54,7 +60,39 @@ export class VariantRetriverService {
   }
 
 
+  /**
+   * This sets current selected Options by user say (small, red, full-sleves)
+   * { tsize: 'small', tcolor: 'red' }
+   * if new currSelectedOptionType is tcolor and the value is blue the 
+   * previous one will get overidden
+   * like { tsize: 'small', tcolor: 'red' } => { tsize: 'small', tcolor: 'blue' }
+   *
+   */
+  setCurrentSelectedOptions() {
+    const currSelectedOptionType = this.currSelectedOption.value
+      .optionValue
+      .option_type_name;
+    this.currentSelectedOptions[currSelectedOptionType] = this.currSelectedOption.key;
+  }
+
+  /**
+   * It creates Custom selected Options extracting the options selected by user from
+   *  customOptionTypesHash
+   * say user selected {tsize: 'small'} this will extract the red option from 
+   * the global option types hash {'tsize': [small: {etc..}, large: {etc..}, medium: {etc..}]}
+   *
+   */
   createCustomSelectedOptions() {
+    /**
+     * currentSelectedOptions: {tsize: 'small', tcolor: 'red'}
+     * currentSelectedOptions: {} at first.. keeps
+     * filling up on iteration of currentSelectedOptions
+     *
+     * this.customOptionTypesHash[key][this.currentSelectedOptions[key]] = 
+     * { optionValue: SomeObject, varaintIds: {etc etc} }
+     *
+     *
+     ** */
     for (const key in this.currentSelectedOptions) {
       if (this.currentSelectedOptions.hasOwnProperty(key)) {
         this.customSelectedOptions[this.currentSelectedOptions[key]] =
@@ -62,7 +100,12 @@ export class VariantRetriverService {
       }
     };
   }
-
+  /**
+   * Makes a currentVaraintIds from the set of customSelectedOptions
+   * {'small': {OptionValue: Object, variantIds: [etc etc]}, 'red': {...}}
+   * @return: array of arrays of varaintIds
+   * e.g: [[1,2,3,4], [7,8,9,1]]
+   */
   setCombinedVariantIds() {
     const temp = [];
     for (const key in this.customSelectedOptions) {
@@ -76,6 +119,14 @@ export class VariantRetriverService {
       .push(temp);
   }
 
+  /**
+   * Gets a Unique variantId from the selectedOptions by user
+   * by intersecting the arrays in currentvaraintIds
+   * Example: [[1,2,3,4], [1,5,6,7]]
+   * returns [1] from which we take first index so it is 1;
+   * if the array only contains [[2,3,4]] then it will return 
+   * the first element i.e 2;
+   */
   getVariantId() {
     // As scoped variable is not accessible
     // inside filter function hence the tempArr declaration;
@@ -88,24 +139,24 @@ export class VariantRetriverService {
   }
 
 
+  /**
+   * Parses Varaint Id if null then sets the first
+   * variantId in the array as the varaintId to return;
+   */
   parseVariantId() {
     if (this.variantId === null || this.variantId === undefined) {
       this.variantId = this.currSelectedOption.value.variantIds[0];
     }
   }
 
-
+  /**
+   * Gets the Varaint From the Product by using the varaintId;
+   * else returns null if not present;
+   */
   getVariantFromProduct() {
     const result = this.product.variants
       .filter(v => { return v.id === parseInt(this.variantId, 10); });
     this.variant = result ? result[0] : null;
-  }
-
-  setCurrentSelectedOptions() {
-    const currSelectedOptionType = this.currSelectedOption.value
-      .optionValue
-      .option_type_name;
-    this.currentSelectedOptions[currSelectedOptionType] = this.currSelectedOption.key;
   }
 
   setCorrespondingOptions() {
