89b991ce26568d5d634d05f142cf3d58fb396432
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-04-06 08:02:39 +0530

    importing observable add in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,10 @@ import { CheckoutFooterComponent } from './layout/checkout-footer/checkout-foote
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/finally';
+import 'rxjs/add/observable/of';
 
 @NgModule({
   declarations: [
