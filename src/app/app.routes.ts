38fe4b8d06f433db9e7c84d50de7f447df0282ab
Ashish Singh <aitashish173@gmail.com>
2017-07-04 12:37:39 +0530

    Fix for issue #54
    * Refactoring for router code
    * Fixed list styling BUG.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,23 @@ import { CanActivateViaAuthGuard } from './core/guards/auth.guard';
 
 
 export const routes: Routes = [
-  { path: '', loadChildren: './home/index#HomeModule' },
-  { path: 'checkout', loadChildren: './checkout/checkout.module#CheckoutModule' },
+  {
+    path: '',
+    loadChildren: './home/index#HomeModule' },
+  {
+    path: 'checkout',
+    loadChildren: './checkout/checkout.module#CheckoutModule' },
   {
     path: 'user',
     loadChildren: './user/index#UserModule',
     canActivate: [ CanActivateViaAuthGuard ]
   },
-  { path: 'product', loadChildren: './product/index#ProductModule' },
-  { path: 'auth', loadChildren: './auth/auth.module#AuthModule' }
+  {
+    path: 'product',
+    loadChildren: './product/index#ProductModule'
+  },
+  {
+    path: 'auth',
+    loadChildren: './auth/auth.module#AuthModule'
+  }
 ];
