a3c1e61c4d66fdbc999bdebdf0e3a466e6d6f5f3
Chandra Shekhar <shekharindian099@gmail.com>
2018-04-25 15:16:41 +0530

    add favorite product toggle method (#100)
    add favorite product functionality
    
    remove from favorite functionality
    
    remove unmark as favorite option from product detail page
    
    indentation and bug fixes
    
    use async pipe in view only once

---
+++ src/app/user/components/favorite-products/favorite-product-list-item/favorite-product-list-item.component.ts
@@ -0,0 +1,32 @@
+import { UserActions } from './../../../actions/user.actions';
+import { AppState } from './../../../../interfaces';
+import { ProductService } from './../../../../core/services/product.service';
+import { environment } from './../../../../../environments/environment';
+import { Product } from './../../../../core/models/product';
+import { Component, OnInit, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-favorite-product-list-item',
+  templateUrl: './favorite-product-list-item.component.html',
+  styleUrls: ['./favorite-product-list-item.component.scss']
+})
+export class FavoriteProductListItemComponent implements OnInit {
+  @Input() product: Product;
+
+  constructor(
+    private productService: ProductService,
+    private store: Store<AppState>,
+    private userActions: UserActions
+  ) { }
+
+  ngOnInit() {
+  }
+
+  removeFromFavorite(id: number) {
+    this.productService.removeFromFavorite(id).subscribe((status) => {
+      this.store.dispatch(this.userActions.removeFromFavoriteProducts(id));
+    });
+  }
+
+}
