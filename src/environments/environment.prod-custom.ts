d0be7375cda90495983c87c050e3cdbce2d6a15f
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-04-27 16:34:06 +0530

    updated prod urls in env

--- src/environments/environment.prod-custom.ts
+++ src/environments/environment.prod-custom.ts
@@ -2,7 +2,7 @@ import { CUSTOM_CONFIG } from '../config/custom';
 
 export const environment = {
   production: false,
-  apiEndpoint: 'http://18.216.147.38/',
+  apiEndpoint: CUSTOM_CONFIG.prodApiEndpoint,
   appName: CUSTOM_CONFIG.appName,
   config: CUSTOM_CONFIG
 };
