b21afba294e4ef2344cd9a4b9bdceed317b2a4df
Pankaj Kumar Rawat <pankajrawat19sept@gmail.com>
2018-05-03 13:24:19 +0530

    ui fixes for landing page (#121)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { DEFAULT_CONFIG } from './../config/default';
 
 export const environment = {
   production: false,
-  apiEndpoint: 'http://18.188.226.168/',
+  apiEndpoint: 'http://localhost:3000/',
   appName: DEFAULT_CONFIG.appName,
   config: DEFAULT_CONFIG
 };
