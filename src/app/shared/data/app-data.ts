835df34725386c29353ea62956ffcf9d413d871a
gopalshimpi <gopalshimpi@gmail.com>
2018-05-03 11:35:13 +0530

    app-data for https videos (#119)

--- src/app/shared/data/app-data.ts
+++ src/app/shared/data/app-data.ts
@@ -1,27 +1,22 @@
 export const APP_DATA = {
   videos: [
     {
-      link: 'http://techslides.com/demos/sample-videos/small.webm',
-      type: 'video/webm',
-      title: 'sample vedio'
-    },
-    {
-      link: 'http://dl3.webmfiles.org/big-buck-bunny_trailer.webm',
+      link: 'https://dl3.webmfiles.org/big-buck-bunny_trailer.webm',
       type: 'video/webm',
       title: 'bunny vedio'
     },
     {
-      link: 'http://dl3.webmfiles.org/elephants-dream.webm',
+      link: 'https://dl3.webmfiles.org/elephants-dream.webm',
       type: 'video/webm',
       title: 'sample vedio'
     },
     {
-      link: 'http://dl3.webmfiles.org/elephants-dream.webm',
+      link: 'https://dl3.webmfiles.org/elephants-dream.webm',
       type: 'video/webm',
       title: 'sample vedio'
     },
     {
-      link: 'http://dl3.webmfiles.org/elephants-dream.webm',
+      link: 'https://dl3.webmfiles.org/elephants-dream.webm',
       type: 'video/webm',
       title: 'sample vedio'
     }
