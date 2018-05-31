e6004a8252fd932e334d423a181737480440d7a5
jaypal <jaypal@aviabird.com>
2018-05-31 19:38:40 +0530

    Work on header

--- src/app/layout/header/components/categories-menu-dropdown/categories-menu-dropdown.component.ts
+++ src/app/layout/header/components/categories-menu-dropdown/categories-menu-dropdown.component.ts
@@ -12,9 +12,7 @@ import {
 } from '@angular/animations';
 
 import { URLSearchParams } from '@angular/http'
-import { TemplateRef } from '@angular/core';
-import { BsModalService } from 'ngx-bootstrap/modal';
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
 
 @Component({
   selector: 'app-categories-menu-dropdown',
@@ -44,24 +42,17 @@ export class CategoriesMenuDropdownComponent implements OnInit {
   queryParams: any;
   show = false;
   backBtnShow = false;
-  modalRef: BsModalRef;
   get stateName() {
     return this.show ? 'show' : 'hide'
   }
   constructor(
     private route: ActivatedRoute,
-    private searchActions: SearchActions,private modalService: BsModalService,
+    private searchActions: SearchActions,
     private store: Store<AppState>) {
     this.route.queryParams
       .subscribe(params => {
         this.queryParams = params;
       });
-  }
-  openModalWithClass(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(
-      template,
-      Object.assign({}, { class: 'cat-mobile' })
-    );
   }
   ngOnInit() {
 
