6d363bdc82592e63213b3b4d31f252f27ef0d3f2
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-06-21 11:18:54 +0530

    added service worked, custom preload strategy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,25 +5,33 @@ import { CanActivateViaAuthGuard } from './core/guards/auth.guard';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: './landing/landing.module#LandingModule'
+    loadChildren: './landing/landing.module#LandingModule',
+    data: { preload: true, delay: true },
   },
   {
     path: 'search',
-    loadChildren: './home/index#HomeModule' },
+    loadChildren: './home/index#HomeModule',
+    data: { preload: false, delay: false },
+  },
   {
     path: 'checkout',
-    loadChildren: './checkout/checkout.module#CheckoutModule' },
+    loadChildren: './checkout/checkout.module#CheckoutModule',
+    data: { preload: true, delay: true },
+  },
   {
     path: 'user',
     loadChildren: './user/index#UserModule',
-    canActivate: [ CanActivateViaAuthGuard ]
+    canActivate: [ CanActivateViaAuthGuard ],
+    data: { preload: false, delay: false },
   },
   {
     path: 'auth',
-    loadChildren: './auth/auth.module#AuthModule'
+    loadChildren: './auth/auth.module#AuthModule',
+    data: { preload: false, delay: false },
   },
   {
     path: '',
-    loadChildren: './product/product.module#ProductModule'
+    loadChildren: './product/product.module#ProductModule',
+    data: { preload: false, delay: false },
   }
 ];
--- src/app/checkout/checkout.routes.ts
+++ src/app/checkout/checkout.routes.ts
@@ -3,7 +3,7 @@ import { PaymentComponent } from './payment/payment.component';
 import { AddressComponent } from './address/address.component';
 import { CartComponent } from './cart/cart.component';
 
-export const CheckoutRoutes = [
+export const routes = [
   { path: '', redirectTo: 'cart', pathMatch: 'full' },
   { path: 'cart', component: CartComponent },
   { path: 'address', component: AddressComponent },
