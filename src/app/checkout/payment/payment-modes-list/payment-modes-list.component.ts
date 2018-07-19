3a9759d7233508218b2bab25caed89fb891ccfeb
Gopal <gopalshimpi@gmail.com>
2018-07-20 00:23:46 +0530

    Added pincode check & order success page fixes

--- src/app/checkout/payment/payment-modes-list/payment-modes-list.component.ts
+++ src/app/checkout/payment/payment-modes-list/payment-modes-list.component.ts
@@ -11,6 +11,7 @@ import { CheckoutService } from './../../../core/services/checkout.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Address } from '../../../core/models/address';
 import { environment } from '../../../../environments/environment';
+import { ToastrService } from '../../../../../node_modules/ngx-toastr';
 
 @Component({
   selector: 'app-payment-modes-list',
@@ -22,18 +23,21 @@ export class PaymentModesListComponent implements OnInit {
   @Input() paymentAmount: number;
   @Input() orderNumber: number;
   @Input() address: Address;
+  isShippeble: boolean;
 
   paymentModes: PaymentMode[];
   selectedMode: PaymentMode = new PaymentMode;
   isAuthenticated: boolean;
   showOrderSuccess = false;
-  free_shipping_order_amount = environment.config.free_shipping_order_amount
+  free_shipping_order_amount = environment.config.free_shipping_order_amount;
+  currency = environment.config.currency_symbol;
 
   constructor(private checkoutService: CheckoutService,
     private paymentService: PaymentService,
     private router: Router,
     private store: Store<AppState>,
-    private checkoutActions: CheckoutActions) {
+    private checkoutActions: CheckoutActions,
+    private toastyService: ToastrService) {
     this.store.select(getAuthStatus).subscribe((auth) => {
       this.isAuthenticated = auth;
     });
@@ -53,21 +57,31 @@ export class PaymentModesListComponent implements OnInit {
         this.paymentModes = payment.payment_methods;
         this.selectedMode = this.paymentService.setCODAsSelectedMode(this.paymentModes);
       });
-
   }
 
   makePaymentCod() {
     const paymentModeId = this.selectedMode.id;
-    const shipping_pincode = this.address.zipcode
-    
-    this.checkoutService.createNewPayment(paymentModeId, this.paymentAmount).pipe(
-      tap(() => {
-        this.store.dispatch(this.checkoutActions.orderCompleteSuccess());
-        this.redirectToNewPage();
-        this.checkoutService.createEmptyOrder()
-          .subscribe();
-      }))
-      .subscribe();
+    const shipping_pincode = (this.address.zipcode)
+    this.checkoutService.shipmentAvailability(+shipping_pincode)
+      .subscribe((res: any) => {
+        this.isShippeble = res.available
+        if (this.isShippeble && this.paymentAmount >= this.free_shipping_order_amount) {
+          this.checkoutService.createNewPayment(paymentModeId, this.paymentAmount).pipe(
+            tap(() => {
+              this.store.dispatch(this.checkoutActions.orderCompleteSuccess());
+              this.redirectToNewPage();
+              this.checkoutService.createEmptyOrder()
+                .subscribe();
+            }))
+            .subscribe();
+        } else {
+          if (this.paymentAmount < this.free_shipping_order_amount) {
+            this.toastyService.error(`COD is not available for Order amount less than ${this.currency} ${this.free_shipping_order_amount}.`, "Order Amount");
+          } else if (!this.isShippeble) {
+            this.toastyService.error(`COD is not available for pincode ${shipping_pincode}.`, "Pincode");
+          }
+        }
+      })
   }
 
   makePaymentPayubiz() {
