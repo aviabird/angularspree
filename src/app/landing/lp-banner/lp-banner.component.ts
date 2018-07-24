0cd21078c9c90e2fd4d207309331fa16100e7417
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-07-25 01:38:54 +0530

    added ngu carousel

--- src/app/landing/lp-banner/lp-banner.component.ts
+++ src/app/landing/lp-banner/lp-banner.component.ts
@@ -1,5 +1,6 @@
 import { environment } from './../../../environments/environment';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { NguCarousel } from '@ngu/carousel';
 
 @Component({
   selector: 'app-lp-banner',
@@ -9,9 +10,23 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class LpBannerComponent implements OnInit {
   banners = environment.config.landing_page_banner;
+  public carouselOne: NguCarousel;
   constructor() { }
 
   ngOnInit() {
+    this.carouselOne = {
+      grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+      slide: 1,
+      speed: 400,
+      interval: 4000,
+      point: {
+        visible: true
+      },
+      load: 2,
+      touch: true,
+      loop: true,
+      custom: 'banner'
+    }
   }
 
 }
