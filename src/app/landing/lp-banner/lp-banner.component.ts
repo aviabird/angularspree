fa4e4ac6b4ebac73b61e30365343ec09945933af
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-07-25 14:29:20 +0530

    refactored carousel usage

--- src/app/landing/lp-banner/lp-banner.component.ts
+++ src/app/landing/lp-banner/lp-banner.component.ts
@@ -11,52 +11,22 @@ import { NguCarousel } from '@ngu/carousel';
 export class LpBannerComponent implements OnInit {
   banners = environment.config.landing_page_banner;
   public carouselOne: NguCarousel;
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.carouselOne = {
-      grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+      grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
       slide: 1,
       speed: 400,
       interval: 4000,
       point: {
         visible: true,
-        pointStyles: `
-          .ngucarouselPoint {
-            list-style-type: none;
-            padding: 3px;
-            white-space: nowrap;
-            overflow: auto;
-            position: relative;
-            bottom: 27px;
-            left: 0;
-            box-sizing: border-box;
-            background-color: white;
-            margin: 0 auto -28px;
-            align-self: center;
-            width: fit-content;
-            border-radius: 5px 5px 0 0;
-          }
-          .ngucarouselPoint li {
-            display: inline-block;
-            border-radius: 999px;
-            background-color: rgba(0, 0, 255, 0.55);
-            padding: 5px;
-            margin: 0 3px;
-            transition: .4s ease all;
-          }
-          .ngucarouselPoint li.active {
-              background-color: white;
-              border: 1px solid rgba(0, 0, 255, 0.55);
-              width: 10px;
-          }
-        `
+        pointStyles: '.ngucarouselPoint {}' // To apply our modification
       },
       load: 2,
       touch: true,
       loop: true,
       custom: 'banner'
-    }
+    };
   }
-
 }
