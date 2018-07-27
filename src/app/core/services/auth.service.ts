db4500bb23099850b05f60c1732b6cf18f41db73
Gopal B Shimpi <gopalshimpi@gmail.com>
2018-07-27 10:52:15 +0530

    Landing page & auth service fixes (#210)
    Why ?
    Fixes
    
    Landing page content loading.(Todays Deals)
    Review page changes like hiding show review bar is not reviews to show.
    Message change for failed payment.
    Added error handling for social login
    This change addresses the need by:
    All bug noticed are fixed.
    
    [delivers #159343732]

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,17 +1,17 @@
-import { of as observableOf, Observable } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
-import { AppState } from '../../interfaces';
-import { Store } from '@ngrx/store';
-import { AuthActions } from '../../auth/actions/auth.actions';
-import { AuthService as OauthService } from 'ng2-ui-auth';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { Authenticate, User } from '../models/user';
-import { delay } from 'q';
-import { HttpRequest } from '@angular/common/http/src/request';
-import { ToastrService, ActiveToast } from 'ngx-toastr';
+import { of as observableOf, Observable } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { AppState } from "../../interfaces";
+import { Store } from "@ngrx/store";
+import { AuthActions } from "../../auth/actions/auth.actions";
+import { AuthService as OauthService } from "ng2-ui-auth";
+import { HttpHeaders, HttpClient } from "@angular/common/http";
+import { Authenticate, User } from "../models/user";
+import { delay } from "q";
+import { HttpRequest } from "@angular/common/http/src/request";
+import { ToastrService, ActiveToast } from "ngx-toastr";
 
 @Injectable()
 export class AuthService {
@@ -42,15 +42,15 @@ export class AuthService {
    */
   login({ email, password }: Authenticate): Observable<User> {
     const params = { spree_user: { email, password } };
-    return this.http.post<User>('login.json', params).pipe(
+    return this.http.post<User>("login.json", params).pipe(
       map(user => {
         this.setTokenInLocalStorage(user);
         this.store.dispatch(this.actions.loginSuccess());
         return user;
       }),
       tap(
-        _ => this.router.navigate(['/']),
-        user => this.toastrService.error(user.error.error, 'ERROR!')
+        _ => this.router.navigate(["/"]),
+        user => this.toastrService.error(user.error.error, "ERROR!")
       )
     );
     // catch should be handled here with the http observable
@@ -69,7 +69,7 @@ export class AuthService {
    */
   register(data: User): Observable<User> {
     const params = { spree_user: data };
-    return this.http.post<User>('auth/accounts', params).pipe(
+    return this.http.post<User>("auth/accounts", params).pipe(
       map(user => {
         this.setTokenInLocalStorage(user);
         this.store.dispatch(this.actions.loginSuccess());
@@ -77,7 +77,7 @@ export class AuthService {
       }),
       tap(
         _ => _,
-        _ => this.toastrService.error('Invalid/Existing data', 'ERROR!!')
+        _ => this.toastrService.error("Invalid/Existing data", "ERROR!!")
       )
     );
     // catch should be handled here with the http observable
@@ -94,20 +94,18 @@ export class AuthService {
    * @memberof AuthService
    */
   forgetPassword(data: User): Observable<any> {
-    return this.http
-      .post('auth/passwords', { spree_user: data })
-      .pipe(
-        map(_ =>
-          this.toastrService.success(
-            'Password reset link has be sent to your email.',
-            'Success'
-          )
-        ),
-        tap(
-          _ => _,
-          _ => this.toastrService.error('Not a valid email/user', 'ERROR!!')
+    return this.http.post("auth/passwords", { spree_user: data }).pipe(
+      map(_ =>
+        this.toastrService.success(
+          "Password reset link has be sent to your email.",
+          "Success"
         )
-      );
+      ),
+      tap(
+        _ => _,
+        _ => this.toastrService.error("Not a valid email/user", "ERROR!!")
+      )
+    );
   }
 
   /**
@@ -123,13 +121,13 @@ export class AuthService {
       .pipe(
         map(_ =>
           this.toastrService.success(
-            'Password updated success fully!',
-            'Success'
+            "Password updated success fully!",
+            "Success"
           )
         ),
         tap(
           _ => _,
-          _ => this.toastrService.error('Unable to update password', 'ERROR!')
+          _ => this.toastrService.error("Unable to update password", "ERROR!")
         )
       );
   }
@@ -142,7 +140,9 @@ export class AuthService {
    * @memberof AuthService
    */
   authorized(): Observable<any> {
-    return this.http.get('auth/authenticated').pipe(map((res: Response) => res));
+    return this.http
+      .get("auth/authenticated")
+      .pipe(map((res: Response) => res));
     // catch should be handled here with the http observable
     // so that only the inner obs dies and not the effect Observable
     // otherwise no further login requests will be fired
@@ -157,10 +157,10 @@ export class AuthService {
    * @memberof AuthService
    */
   logout() {
-    return this.http.get('logout.json').pipe(
+    return this.http.get("logout.json").pipe(
       map((res: Response) => {
         // Setting token after login
-        localStorage.removeItem('user');
+        localStorage.removeItem("user");
         this.store.dispatch(this.actions.logoutSuccess());
         return res;
       })
@@ -174,18 +174,19 @@ export class AuthService {
    * @memberof AuthService
    */
   getTokenHeader(request: HttpRequest<any>): HttpHeaders {
-    const user: User = ['undefined', null]
-      .indexOf(localStorage.getItem('user')) === -1 ?
-      JSON.parse(localStorage.getItem('user')) : {};
+    const user: User =
+      ["undefined", null].indexOf(localStorage.getItem("user")) === -1
+        ? JSON.parse(localStorage.getItem("user"))
+        : {};
 
     return new HttpHeaders({
-      'Content-Type': request.headers.get('Content-Type') || 'application/json',
-      'token-type': 'Bearer',
-      'access_token': user.access_token || [],
-      'client': user.client || [],
-      'uid': user.uid || [],
-      'Auth-Token': user.spree_api_key || [],
-      'ng-api': 'true'
+      "Content-Type": request.headers.get("Content-Type") || "application/json",
+      "token-type": "Bearer",
+      access_token: user.access_token || [],
+      client: user.client || [],
+      uid: user.uid || [],
+      "Auth-Token": user.spree_api_key || [],
+      "ng-api": "true"
     });
   }
 
@@ -199,7 +200,7 @@ export class AuthService {
    */
   private setTokenInLocalStorage(user_data: any): void {
     const jsonData = JSON.stringify(user_data);
-    localStorage.setItem('user', jsonData);
+    localStorage.setItem("user", jsonData);
   }
 
   socialLogin(provider: string) {
@@ -209,7 +210,8 @@ export class AuthService {
         return user;
       }),
       catchError(_ => {
-        return observableOf('Social login failed');
+        this.toastrService.error("Social login failed", "ERROR!");
+        return observableOf("Social login failed");
       })
     );
   }
