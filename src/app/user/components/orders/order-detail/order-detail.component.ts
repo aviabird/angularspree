dc1351ee66b853cbe8a95e63297f631ed32a3817
Gopal B Shimpi <gopalshimpi@gmail.com>
2018-08-01 11:17:06 +0530

    Retry Payment & Sortfilters (#225)
    Why?
    
    Feature
    
    Added Sorting based on Avg.Customer Review & Most Review for products search.
    Added retry payment option for orderlist.
    This change addresses the need by:
    
    Now User can sort the search result of products by avg.rating, review_count.
    
    This sort filters are available in products search page.
    User can have retry payment option for failed payment in orders details page of respective user.
    
    [delivers #159427862,#159424402]

--- src/app/user/components/orders/order-detail/order-detail.component.ts
+++ src/app/user/components/orders/order-detail/order-detail.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState } from '../../../../interfaces';
-import { UserActions } from '../../../actions/user.actions';
 import { UserService } from '../../../services/user.service';
 import { Order } from '../../../../core/models/order';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription ,  Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { environment } from '../../../../../environments/environment';
 import { LineItem } from '../../../../core/models/line_item';
+import { CheckoutService } from '../../../../core/services/checkout.service';
 
 @Component({
   selector: 'app-order-detail',
@@ -19,12 +17,14 @@ export class OrderDetailComponent implements OnInit, OnDestroy {
   orderSubscription$: Subscription;
   orderNumber: string;
   order: Order;
+  title = environment.appName;
+  payubiz = environment.config.PaymentMethodPayubiz
 
   constructor(
     private userService: UserService,
-    private route: ActivatedRoute
-  ) {
-  }
+    private route: ActivatedRoute,
+    private checkoutService: CheckoutService
+  ) { }
 
   ngOnInit() {
     this.routeSubscription$ = this.route.params.subscribe(
@@ -32,9 +32,9 @@ export class OrderDetailComponent implements OnInit, OnDestroy {
         this.orderNumber = params['number'];
         this.orderSubscription$ =
           this.userService
-          .getOrderDetail(this.orderNumber)
-          .subscribe(order => this.order = order);
-     }
+            .getOrderDetail(this.orderNumber)
+            .subscribe(order => this.order = order);
+      }
     );
   }
 
@@ -47,4 +47,11 @@ export class OrderDetailComponent implements OnInit, OnDestroy {
     this.orderSubscription$.unsubscribe();
   }
 
+  retryPayment(order: Order) {
+    this.checkoutService.makePayment(+order.total, order.bill_address, order.number)
+      .subscribe((response: any) => {
+        response = response
+        window.open(response.url, '_self');
+      });
+  }
 }
