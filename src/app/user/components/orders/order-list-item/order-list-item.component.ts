e4fcc37f621a22fdcb18cf81904e468a661eb045
jaypal412 <jaypal@aviabird.com>
2018-08-03 10:36:49 +0530

    refactored metainfo usage

--- src/app/user/components/orders/order-list-item/order-list-item.component.ts
+++ src/app/user/components/orders/order-list-item/order-list-item.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Order } from '../../../../core/models/order';
 import { LineItem } from '../../../../core/models/line_item';
+import { environment } from '../../../../../environments/environment.dev-ng-spree';
+import { CheckoutService } from '../../../../core/services/checkout.service';
+
 @Component({
   selector: 'app-order-list-item',
   templateUrl: './order-list-item.component.html',
@@ -8,13 +11,21 @@ import { LineItem } from '../../../../core/models/line_item';
 })
 export class OrderListItemComponent implements OnInit {
   @Input() orders;
+  payubiz = environment.config.PaymentMethodPayubiz
 
-  constructor() { }
+  constructor( private checkoutService: CheckoutService) { }
 
   ngOnInit() {
   }
 
   getProductImageUrl(line_item: LineItem) {
     return line_item.variant.images[0].small_url;
+  }
+  retryPayment(order: Order) {
+    this.checkoutService.makePayment(+order.total, order.bill_address, order.number)
+      .subscribe((response: any) => {
+        response = response
+        window.open(response.url, '_self');
+      });
   }
 }
