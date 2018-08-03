7c43cd0d437a75d0edcae06e64eb0ae10b02f29d
jaypal412 <39181626+jaypal412@users.noreply.github.com>
2018-08-03 14:53:40 +0530

    Ui changes/fixes (#226)
    Why?
    
    Feature:
    
    Work on the order profile page.
    Work on the order detail page.
    Work on order success page.
    This change addresses the need by:
    
    The UI of the all order page is not good enough so we have changed it little more.
    
    [delivers #159494692]

--- src/app/user/components/orders/order-list-item/order-list-item.component.ts
+++ src/app/user/components/orders/order-list-item/order-list-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Order } from '../../../../core/models/order';
+import { LineItem } from '../../../../core/models/line_item';
+import { environment } from '../../../../../environments/environment.dev-ng-spree';
+import { CheckoutService } from '../../../../core/services/checkout.service';
 
 @Component({
   selector: 'app-order-list-item',
@@ -7,13 +11,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class OrderListItemComponent implements OnInit {
   @Input() orders;
+  payubiz = environment.config.PaymentMethodPayubiz
 
-  constructor() { }
+  constructor( private checkoutService: CheckoutService) { }
 
   ngOnInit() {
   }
 
-  getProductImageUrl(url) {
-    return url;
+  getProductImageUrl(line_item: LineItem) {
+    return line_item.variant.images[0].small_url;
+  }
+  retryPayment(order: Order) {
+    this.checkoutService.makePayment(+order.total, order.bill_address, order.number)
+      .subscribe((response: any) => {
+        response = response
+        window.open(response.url, '_self');
+      });
   }
 }
