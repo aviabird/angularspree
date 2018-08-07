5bd59dfa954ea08e7fd3c762e8657c28eeb06446
Gopal B Shimpi <gopalshimpi@gmail.com>
2018-08-07 19:11:29 +0530

    added profile fixes (#235)
    Why?
    
    Enhancement
    
    User profile page was not showing the the currently selected option from profile in mobile view.
    This change addresses the need by:
    
    User will get the selected option like My Account > My Profile, My Account > My Orders etc.
    [delivers #159589280]

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  selectedProfileMenu: any;
 
-  constructor() { }
+  profileOption = [
+    { name: 'My Profile', url: '/user/profile' },
+    { name: 'My Orders', url: '/user/orders' },
+    { name: 'My Address', url: '/user/addresses' },
+    { name: 'My Favorite Products', url: '/user/favorite-products' }
+  ]
+  constructor(private router: Router) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showSelectedMenu(event.url);
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
+  showSelectedMenu(url) {
+    this.selectedProfileMenu = this.profileOption.find(element =>
+      element.url.includes(url))
+  }
 }
