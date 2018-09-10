9147286b820d8b0f8966aec93a75100c03919fda
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-09-10 13:37:39 +0530

    removed custom config from gitignore

---
+++ src/config/custom/meta-data.ts
@@ -0,0 +1,18 @@
+export const CUSTOM_META_DATA = {
+  // Meta information
+  // Global
+  metaInfo: {
+    googleSiteVerification: 'nG3sJH9BDF5kFOQrsvNkx_xbWVEO_wqNiE5dirqo3Yk',
+    title: `AngularSpree Plug and play frontend application for SPREE
+    E-Commerce API built with ❤️ using Angular2, Redux, Observables & ImmutableJs.`,
+    description: `AngularSpree Plug and play frontend application for SPREE E-Commerce
+    API built with ❤️ using Angular6, Redux, Observables & ImmutableJs.`,
+    // Page specific
+    landingPage: {
+      title: `AngularSpree Plug and play frontend application for SPREE
+      E-Commerce API built with ❤️ using Angular2, Redux, Observables & ImmutableJs.`,
+      description: `AngularSpree Plug and play frontend application for SPREE E-Commerce
+      API built with ❤️ using Angular6, Redux, Observables & ImmutableJs.`
+    },
+  },
+}
