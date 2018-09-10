47ba03e04c8737fea76738a0e4c97e83226aabad
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-09-10 13:37:39 +0530

    removed custom config from gitignore

---
+++ src/config/custom/app-data.ts
@@ -0,0 +1,107 @@
+const cdn_url = 'https://res.cloudinary.com/aviabird/image/upload/q_60/v1529433193/angularspree/';
+
+export const CUSTOM_APP_DATA = {
+  landing_page_banner: [
+    {
+      image_link: `${cdn_url}banner-1.jpg`,
+      link_url: '#'
+    },
+    {
+      image_link: `${cdn_url}banner-2.jpg`,
+      link_url: '#'
+    },
+    {
+      image_link: `${cdn_url}banner-3.jpg`,
+      link_url: '#'
+    },
+    {
+      image_link: `${cdn_url}banner-4.jpg`,
+      link_url: '#'
+    }
+  ],
+  promo_banner: {
+    image_link: `${cdn_url}secondary-banner-1.jpg`,
+    link_url: '#'
+  },
+  category_banner: {
+    Living: {
+      image_link: `${cdn_url}banner-1.jpg`,
+      link_url: '#'
+    },
+    Bedroom: {
+      image_link: `${cdn_url}banner-2.jpg`,
+      link_url: '#'
+    },
+    Dining: {
+      image_link: `${cdn_url}banner-3.jpg`,
+      link_url: '#'
+    },
+    Study: {
+      image_link: `${cdn_url}banner-4.jpg`,
+      link_url: '#'
+    }
+  },
+  Deals: {
+    type: 'Today\'s Deals'
+  },
+
+  footer_page_links: [
+    {
+      name: 'About Us',
+      link_url: 'about'
+    },
+    {
+      name: 'Blog',
+      link_url: 'blog'
+    },
+    {
+      name: 'Return Policy',
+      link_url: 'return_policy'
+    },
+    {
+      name: 'FAQs',
+      link_url: 'faqs'
+    },
+    {
+      name: 'Testimonials',
+      link_url: 'testimonials'
+    }
+  ],
+  footer_social_links: [
+    {
+      link_url: 'https://twitter.com/angularspree',
+      name: 'Twitter',
+      icon: 'fa fa-twitter-square'
+    },
+    {
+      link_url: 'https://www.instagram.com/angularspree/',
+      name: 'Instagram',
+      icon: 'fa fa-instagram'
+    },
+    {
+      link_url:
+        'https://plus.google.com/b/110371544800340671090/110371544800340671090',
+      name: 'Google +',
+      icon: 'fa fa-google-plus-square'
+    },
+    {
+      link_url: 'https://in.pinterest.com/angularspree/',
+      name: 'Pinterest',
+      icon: 'fa fa-pinterest-square'
+    },
+    {
+      link_url: 'https://www.facebook.com/angularspree/',
+      name: 'Facebook',
+      icon: 'fa fa-facebook-square'
+    },
+    {
+      link_url: 'https://www.youtube.com/channel/UCFBvY3QxKAKBAI_chAzRpjQ',
+      name: 'Youtube',
+      icon: 'fa fa-youtube-square'
+    }
+  ],
+  contact_info: {
+    contact_no: '888-888-xxx',
+    copyright: 'Copyright © 2018 AngularSpree, Inc.'
+  }
+};
---
+++ src/config/custom/custom.ts
@@ -0,0 +1,34 @@
+import { CUSTOM_APP_DATA } from './app-data';
+import { CUSTOM_META_DATA } from './meta-data';
+
+export const DEFAULT_CONFIG = {
+  // Add Your custom configs hereh
+  prodApiEndpoint: 'https://ngspree-api.herokuapp.com/',
+  // prodApiEndpoint: 'http://localhost:3000/',
+  frontEndUrl: 'https://angularspree.firebaseapp.com/',
+  appName: 'Angularspree',
+  fevicon: 'https://via.placeholder.com/350x150',
+  header: {
+    brand: {
+      logo: '/assets/default/logo.svg',
+      name: 'Angularspree',
+      height: '42',
+      width: '140'
+    },
+    searchPlaceholder: 'Find good furniture for me',
+    showGithubRibon: false
+  },
+  showDummyCardInfo: true,
+  // Following are the test crediantials for payubiz payment gateway.
+  payuBizSalt: 'eCwWELxi',
+  payuBizKey: 'gtKFFx',
+  freeShippingAmount: 50,
+  currency_symbol: '$', // USD $
+  PaymentMethodCod: 'COD',
+  PaymentMethodPayubiz: 'Payubiz',
+  defaultPaymentMethod: 'Payubiz',
+  reviewsDisplayLimit: 5,
+
+  ...CUSTOM_APP_DATA,
+  ...CUSTOM_META_DATA
+};
