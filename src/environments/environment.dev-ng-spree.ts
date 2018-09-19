4d87fa144a32e90783851cc9142cb6957965121b
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-09-19 23:47:02 +0530

    dev env url fix

--- src/environments/environment.dev-ng-spree.ts
+++ src/environments/environment.dev-ng-spree.ts
@@ -2,7 +2,7 @@ import { DEFAULT_CONFIG } from '../config/default/default';
 
 export const environment = {
   production: false,
-  apiEndpoint: 'http://localhost:3000',
+  apiEndpoint: 'http://localhost:3000/',
   appName: DEFAULT_CONFIG.appName,
   config: DEFAULT_CONFIG
 };
