ee2958539dd0c9ea3c6ec66d02f55ddec46fadef
Gopal <gopalshimpi@gmail.com>
2018-11-27 14:27:17 +0530

    added fixes

--- src/app/layout/header/components/header-cart/header-cart.component.ts
+++ src/app/layout/header/components/header-cart/header-cart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { CheckoutService } from '../../../../core/services/checkout.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header-cart',
@@ -6,12 +8,23 @@ import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core
   styleUrls: ['./header-cart.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderCartComponent implements OnInit {
+export class HeaderCartComponent implements OnInit, OnDestroy {
   @Input() totalCartItems: number;
   @Input() isMobile;
-  constructor() { }
-  ngOnInit() {
+  subscriptionList$: Array<Subscription> = [];
 
+  constructor(private checkOutService: CheckoutService) { }
+
+  ngOnInit() {}
+
+  getCurrentOrder() {
+    this.subscriptionList$.push(
+      this.checkOutService.fetchCurrentOrder().subscribe()
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptionList$.forEach(sub$ => sub$.unsubscribe());
   }
 
 }
