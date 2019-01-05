f1ddf6bbd468d986c299aa152dbbf7c4ace66df5
Pankaj Kumar Rawat <pankajrawat19sept@gmail.com>
2019-01-06 00:16:25 +0530

    fixed range filter (#318)
    ### Why?
    - Range filter boundaries cannot be reverted back once selected.
    - Due to the API returning the range boundaries depending on the params.
    
    ### This change addresses the need by:
    - Uses the API's max range value, irrespective of what was selected.
    - Fixes the issue with the options integration for ng5-slider.
    - Adds custom CSS override for ng5-slider

--- src/app/modules/search/components/search-filters-container/components/range-filter/range-filter.component.ts
+++ src/app/modules/search/components/search-filters-container/components/range-filter/range-filter.component.ts
@@ -1,3 +1,4 @@
+import { SearchFilter } from './../../../../models/search-param';
 import { Options, LabelType } from 'ng5-slider';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { RangeAgg } from '../../../../models/search-param';
@@ -11,16 +12,35 @@ export class RangeFilterComponent implements OnInit {
   @Input() filter: RangeAgg;
   @Input() symbol: string;
   @Output() rangeSeleted = new EventEmitter<string>();
+  @Input() appliedFilters: Array<SearchFilter>;
+  minValue = 0;
+  maxValue = 100;
   options: Options = {
     floor: 0,
+    ceil: 100,
     translate: (value: number, _: LabelType): string => {
-      return this.symbol + value;
+      return `${this.symbol || ''} ${value}`;
     }
   };
 
   constructor() { }
 
   ngOnInit() {
+    this.options = Object.assign({}, this.options, {
+      ceil: this.filter.max
+    });
+
+    const currentFilter = this.appliedFilters
+      .find(filter => filter.id === this.filter.id);
+
+    if (currentFilter) {
+      const [min, max] = currentFilter.values[0].split(' TO ');
+      this.minValue = parseInt(min, 10);
+      this.maxValue = parseInt(max, 10);
+    } else {
+      this.minValue = this.filter.min;
+      this.maxValue = this.filter.max;
+    }
   }
 
   filterUpdated(event: { value: number; highValue: number; }) {
