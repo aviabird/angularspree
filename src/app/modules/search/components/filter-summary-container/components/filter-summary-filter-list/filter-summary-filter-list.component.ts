c3a78d89395c43e5817ce3611a54d2131ba3e589
Yogesh <yogdeep.k@gmail.com>
2019-02-08 18:41:42 +0530

    Fixes/summary filters (#329)
    * Fixed remove individual fIlter
    
    * Transition added and changed border color

--- src/app/modules/search/components/filter-summary-container/components/filter-summary-filter-list/filter-summary-filter-list.component.ts
+++ src/app/modules/search/components/filter-summary-container/components/filter-summary-filter-list/filter-summary-filter-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { SearchAppliedParams } from '../../../../models/search-param';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { SearchFilter, SearchAppliedParams } from '../../../../models/search-param';
 
 @Component({
   selector: 'app-filter-summary-filter-list',
@@ -8,6 +8,7 @@ import { SearchAppliedParams } from '../../../../models/search-param';
 })
 export class FilterSummaryFilterListComponent implements OnInit {
   @Input() appliedParams: SearchAppliedParams;
+  @Output() filterUpdated = new EventEmitter();
 
   constructor() { }
 
@@ -17,6 +18,41 @@ export class FilterSummaryFilterListComponent implements OnInit {
   get filterList() {
     return this.appliedParams.filters
       .reduce((acc, filter) => acc.concat(filter.values), []);
+  }
+
+
+  updateFilter(updatedVal: any, filterName: string) {
+    console.log(this.updateFilter);
+    const currentAppliedFilters = this.appliedParams.filters;
+    const filterToUpdate = currentAppliedFilters.find(f => f.id === filterName);
+    let newCurrentFilters: Array<SearchFilter>;
+
+    if (filterToUpdate) {
+      const currentValues = filterToUpdate.values;
+      const exists = currentValues.find(v => v === updatedVal);
+      const filteredValues = currentValues.filter(v => v !== updatedVal);
+      const filteredAppliedFilters = currentAppliedFilters.filter(f => f.id !== filterName);
+      newCurrentFilters = [
+        ...filteredAppliedFilters,
+        {
+          ...filterToUpdate,
+          values: exists ? filteredValues : [...filteredValues, updatedVal]
+        }
+      ]
+    } else {
+      newCurrentFilters = [
+        ...currentAppliedFilters,
+        {
+          id: filterName,
+          values: [updatedVal]
+        }
+      ]
+    }
+
+    this.filterUpdated.emit({
+      ...this.appliedParams,
+      filters: newCurrentFilters
+    });
   }
 
 }
