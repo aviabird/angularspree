5705a35f520ff5a3d8541fde8f1da5638a0c69c4
Pankaj Kumar Rawat <pankajrawat19sept@gmail.com>
2022-08-05 13:54:31 +0530

    updated user route (#398)

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,3 +1,5 @@
+import { Routes } from "@angular/router";
+
 import { FavoriteProductsComponent } from './components/favorite-products/favorite-products.component';
 import { OverviewComponent } from './components/overview/overview.component';
 import { UserComponent } from './user.component';
@@ -6,18 +8,11 @@ import { AddressesComponent } from './components/addresses/addresses.component';
 import { OrderDetailComponent } from './components/orders/order-detail/order-detail.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
-export const UserRoutes = [
-  {
-    path: '',
-    component: UserComponent,
-    children: [
-      { path: '', redirectTo: 'orders' },
-      // { path: 'overview', component: OverviewComponent, redirectTo: 'orders' },
-      { path: 'orders', component: OrdersComponent },
-      { path: 'orders/detail/:number', component: OrderDetailComponent },
-      { path: 'addresses', component: AddressesComponent },
-      { path: 'favorite-products', component: FavoriteProductsComponent },
-      { path: 'profile', component: ProfileComponent }
-    ]
-  }
+export const UserRoutes: Routes = [
+  { path: '', redirectTo: 'orders', pathMatch: 'full' },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'orders/detail/:number', component: OrderDetailComponent },
+  { path: 'addresses', component: AddressesComponent },
+  { path: 'favorite-products', component: FavoriteProductsComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
